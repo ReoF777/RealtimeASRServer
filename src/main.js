315336694f3a5c015c507e3673c1fa4822a8e576
ReoF777 <reonafuji@gmail.com>
2025-07-22 16:44:41 +0900

    更新

---
+++ src/main.js
@@ -0,0 +1,27 @@
+let _isStart = true;         // 音声認識を開始したかどうか
+let _lengMode = "Japanese";  // 音声認識の言語モード
+let _infoMode = "simple"     // 接続されたサーバーの音声認識モード
+
+function StartASR () { // 音声認識開始メソッド
+
+}
+
+function StopASR () {  // 音声認識終了メソッド
+
+} 
+
+function GetVolumePressure () { // 音圧取得メソッド
+
+}
+
+function StartWebSocketServer () { // WebSocketサーバーを立てるメソッド
+
+}
+
+function SetMute () { // マイクをミュートモードに設定するメソッド
+
+}
+
+function DrawVolumnGraph () { // 音のグラフを表示するメソッド
+
+}
